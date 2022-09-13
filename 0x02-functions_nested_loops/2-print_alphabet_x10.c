7bc340b7ca583fb37a041552920842f45316a2cd
Addisalem <addisalemgirma13@gmail.com>
2022-09-13 07:13:23 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	
-	for(i = 'a'; i <= 'z'; i++)
+	int j;
+
+	for (j = 0; j <= 10; j++)
 	{
-		_putchar(i);
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
