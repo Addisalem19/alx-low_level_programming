dfbe1ebc3709299aebddd22de34f05eb0f22c597
Addisalem <addisalemgirma13@gmail.com>
2022-09-19 14:06:33 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,70 +1,36 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * _atoi - Convert a string to an integer
- * @s: The string to be converted
+ * @s: input string
  *
  * Return: the converted integer
  */
 int _atoi(char *s)
 {
-	int negative = 0;
-	int start = 0;
-	int foundDigit = 0;
-	int i;
-	int n = 0;
-	int steps = 0;
-	int c;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*(s + count) != '\0')
 	{
-		c = s[i];
-		if (c == '-' && start != 1)
-		{
-			if (negative == 1)
-				negative = 0;
-			else
-				negative = 1;
-		}
-
-		if (_isdigit(c) != 1 && foundDigit == 1)
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
-		if (foundDigit != 1 && _isdigit(c) == 1)
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
-			foundDigit = 1;
-			start = 1;
-			steps = 1;
+			if (size > 0)
+				m *= 10;
+			size++;
 		}
-		if (foundDigit)
-		{
-			n = (n * steps) + (int)(c - 48);
-			steps = 10;
-		}
+		count++;
 	}
-	if (negative == 1)
-		return (n * -1);
-	else
-		return (n);
-}
 
-/**
- * _isdigit - Checks for a digit
- * @c: The character to be checked
- *
- * Return: 1 if c is a digit, 0 otherwise
- */
-int _isdigit(int c)
-{
-	int res, zero, max;
-
-	zero = 48;
-	max = zero + 9;
-
-	if (c >= zero && c <= max)
-		res = 1;
-	else
-		res = 0;
-
-	return (res);
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
