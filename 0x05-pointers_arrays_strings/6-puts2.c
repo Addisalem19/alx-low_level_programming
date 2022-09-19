9cf35a423ee44e3712670f514077753962c69259
Addisalem <addisalemgirma13@gmail.com>
2022-09-19 13:29:57 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, starting with the first cxt
+ * puts2 - prints every other character of a string, starting with
+ * the first character
  * @str: The string with the characters to be printed
  *
  * Return: void
