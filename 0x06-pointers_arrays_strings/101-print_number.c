0ca7b996c948cf53af226ea32a65c20454bf3329
Addisalem <addisalemgirma13@gmail.com>
2022-09-22 13:26:09 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,33 +2,67 @@
  * print_number - prints an integer
  * @n: input integer
  *
- * Return: no return
+ * Return: void
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	int divisor, digit;
+	int base = 10;
+	int places = 9;
+	int isNegative = 0;
+	int isMinInt = 0;
+	int begin = 0;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
-	}
-	else
-	{
-		m = n;
+		isNegative = 1;
+		n = n * -1;
+		isMinInt = n < 0 ? 1 : 0;
+		n = n < 0 ? (n + 1) * -1 : n;
 	}
 
-	d = m;
-	count = 1;
-
-	while (d > 9)
+	while (places >= 0)
 	{
-		d /= 10;
-		count *= 10;
-	}
+		divisor = to_power(base, places);
+		digit = ((n / divisor) % 10);
 
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
+		if (begin == 0 && digit == 0)
+			places--;
+		else if (begin == 0 && digit != 0)
+		{
+			begin = 1;if (isNegative == 1)
+				_putchar('-');
+			_putchar(digit + '0');
+			places--;
+		}
+		else
+		{
+			if (isMinInt == 1 && divisor == 1)
+				++digit;
+			_putchar(digit + '0');
+			places--;
+		}
 	}
+	if (divisor == 1 && begin == 0)
+		_putchar(48);
+}
+
+/**
+ * to_power - Computes the exponential of a number
+ * @n: The number
+ * @base: The power that n is raised to
+ *
+ * Return: The computed power
+ */
+int to_power(int n, int base)
+{
+	int i = 0;
+	int exp = 1;
+
+	while (i < base)
+	{
+		exp *= n;
+		i++;
+	}
+	return (exp);
 }
