dbb002c9d2913ee487976fb70dab465eb866e75a
Addisalem <addisalemgirma13@gmail.com>
2022-09-28 07:52:36 -0700

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,33 +1,47 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if the strings can be
- * considered identical, otherwise return 0
- * @s1: The first string
- * @s2: The second string
+ * _strlen_recursion - returns the length of a string
+ * @s: string
  *
- * Return: int
+ * Return: the length of a string
  */
-int wildcmp(char *s1, char *s2)
+int _strlen_recursion(char *s)
 {
-	int res;
-
-	if (*s1 == '\0' && *s2 == '\0')
-		res = 1;
-	else if (*s1 != *s2 && *s2 != '*')
-		res = 0;
-	else if (*s1 == '\0' && *s2 == '*')
-		res = wildcmp(s1, ++s2);
-	else if (*(s1 + 1) == '\0' && *(s2 + 1) != '\0')
-		res = wildcmp(s1, ++s2);
-	else if (*s1 == *s2 && *(s2 + 1) == '*')
-		res = wildcmp(s1, ++s2);
-	else if (*(s1 + 1) != *(s2 + 1) && *(s2 + 1) == '*')
-		res = wildcmp(++s1, ++s2);
-	else if (*(s1 + 1) != *(s2 + 1) && *s2 == '*')
-		res = wildcmp(++s1, s2);
+	if (*s == '\0')
+		return (0);
 	else
-		res = wildcmp(++s1, ++s2);
+		return (1 + _strlen_recursion(s + 1));
+}
 
-	return (res);
+/**
+ * comparator - compares each character of the string
+ * @s: string
+ * @n1: smallest iterator
+ * @n2: biggest iterator
+ *
+ * Return: 0
+ */
+int comparator(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - detects if a string is a palindrome
+ * @s: string
+ *
+ * Return: 1 if s is a palindrome, 0 if not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
