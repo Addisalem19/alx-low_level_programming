7f0ff9d07ca93780dd01c7284ad99526446421d6
Addisalem <addisalemgirma13@gmail.com>
2022-10-06 11:20:43 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: The block size in byte
+ *
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
